b7bf3d1438e21fb25c441f55e068e2e137690bbd
i_82 <i.82@qq.com>
2017-05-22 11:09:45 +0200

    MYSQL

--- Mew/settings.py
+++ Mew/settings.py
@@ -85,8 +85,15 @@ WSGI_APPLICATION = 'Mew.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MEW',
+        'USER': 'mew',
+        'PASSWORD': 'MMyeqoUMVjfL7Usxbd929',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
