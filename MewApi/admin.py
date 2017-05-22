8a55883547b807d13e60e4eac797678b4b6c8acb
i_82 <i.82@qq.com>
2017-05-22 10:12:18 +0200

    Initial Commit

---
+++ MewApi/admin.py
@@ -0,0 +1,61 @@
+import string
+
+from django.contrib import admin
+from MewApi.models import *
+import random
+
+# Register your models here.
+
+
+class MewVersionAdmin(admin.ModelAdmin):
+    list_display = ("version_string", "enabled", "created_at")
+    search_fields = ["version_string"]
+    list_filter = ["enabled"]
+
+
+class MewDeviceAdmin(admin.ModelAdmin):
+    list_display = ("unique_id", "enabled", "created_at")
+    search_fields = ["unique_id"]
+    list_filter = ["enabled"]
+
+
+class MewCodeAdmin(admin.ModelAdmin):
+    list_display = ("code_value", "bucket", "enabled", "bind_device", "used_at")
+    search_fields = ["code_value", "bind_device"]
+    list_filter = ["enabled", "bucket"]
+    readonly_fields = ["used_at", "bind_device"]
+
+
+class MewCodeBucketAdmin(admin.ModelAdmin):
+    
+    list_display = ("name", "code_count", "created_at")
+    search_fields = ["name"]
+    
+    def save_model(self, request, obj, form, change):
+        code_export = ""
+        obj.save()
+        for i in range(0, obj.code_count):
+            code_n = MewCode()
+            code_n.code_value = ''.join(random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ3456789") for _ in range(12))
+            code_n.bucket = obj
+            code_n.save()
+            code_export += code_n.code_value + "\n"
+        obj.code_export = code_export
+        super(MewCodeBucketAdmin, self).save_model(request, obj, form, change)
+        
+    def get_readonly_fields(self, request, obj=None):
+        if obj is None:
+            return ["code_export"]
+        else:
+            return ["code_export", "code_count"]
+
+
+class MewCertificateAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(MewVersion, MewVersionAdmin)
+admin.site.register(MewDevice, MewDeviceAdmin)
+admin.site.register(MewCode, MewCodeAdmin)
+admin.site.register(MewCodeBucket, MewCodeBucketAdmin)
+admin.site.register(MewCertificate, MewCertificateAdmin)
---
+++ MewApi/models.py
@@ -0,0 +1,179 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class MewVersion(models.Model):
+    class Meta(object):
+        verbose_name = "Version"
+        verbose_name_plural = "Versions"
+
+    id = models.AutoField(
+        primary_key=True,
+        editable=False
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name="Created At",
+        auto_now_add=True
+    )  # OK
+    
+    version_string = models.CharField(
+        verbose_name="Version String",
+        max_length=16,
+        default=""
+    )
+    
+    enabled = models.BooleanField(
+        verbose_name="Enabled",
+        default=True
+    )
+
+    def __unicode__(self):
+        return self.version_string
+
+
+class MewDevice(models.Model):
+    class Meta(object):
+        verbose_name = "Device"
+        verbose_name_plural = "Devices"
+
+    id = models.AutoField(
+        primary_key=True,
+        editable=False
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name="Created At",
+        auto_now_add=True
+    )  # OK
+    
+    unique_id = models.CharField(
+        verbose_name="Unique ID",
+        max_length=40,
+        default="",
+        unique=True,
+    )  # OK
+    
+    enabled = models.BooleanField(
+        verbose_name="Enabled",
+        default=True
+    )
+
+    def __unicode__(self):
+        return self.unique_id
+
+
+class MewCodeBucket(models.Model):
+    class Meta(object):
+        verbose_name = "Code Bucket"
+        verbose_name_plural = "Code Buckets"
+    
+    id = models.AutoField(
+        primary_key=True,
+        editable=False
+    )
+    
+    created_at = models.DateTimeField(
+        verbose_name="Created At",
+        auto_now_add=True
+    )
+    
+    name = models.CharField(
+        verbose_name="Name",
+        max_length=64,
+        unique=True,
+        default="Untitled Bucket"
+    )
+    
+    code_count = models.IntegerField(
+        verbose_name="Code Count",
+        default=100,
+    )
+    
+    code_export = models.TextField(
+        verbose_name="Code Export",
+        blank=True
+    )
+    
+    def __unicode__(self):
+        return self.name
+
+
+class MewCode(models.Model):
+    class Meta(object):
+        verbose_name = "Code"
+        verbose_name_plural = "Codes"
+
+    id = models.AutoField(
+        primary_key=True,
+        editable=False
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name="Created At",
+        auto_now_add=True
+    )  # OK
+    
+    used_at = models.DateTimeField(
+        verbose_name="Used At",
+        blank=True,
+        null=True
+    )  # OK
+
+    code_value = models.CharField(
+        verbose_name="Code Value",
+        max_length=16,
+        default="",
+        unique=True,
+    )  # OK
+    
+    bind_device = models.ForeignKey(
+        MewDevice,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    
+    enabled = models.BooleanField(
+        verbose_name="Enabled",
+        default=True
+    )
+    
+    bucket = models.ForeignKey(
+        MewCodeBucket,
+        editable=False,
+        default=None,
+        on_delete=models.CASCADE
+    )
+
+    def __unicode__(self):
+        return self.code_value
+
+
+class MewCertificate(models.Model):
+    class Meta(object):
+        verbose_name = "Certificate"
+        verbose_name_plural = "Certificates"
+
+    id = models.AutoField(
+        primary_key=True,
+        editable=False
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name="Created At",
+        auto_now_add=True
+    )
+    
+    private_key = models.TextField(
+        verbose_name="Private Key",
+        blank=False
+    )
+    
+    public_key = models.TextField(
+        verbose_name="Public Key",
+        blank=False
+    )
